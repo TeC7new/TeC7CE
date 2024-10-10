80fac717dd2eeabd327678658a5e29717bdd0289
i20ikenaga <120ikenaga@tokuyama.kosen-ac.jp>
2024-10-10 13:37:19 +0900

    Sequencer.vhdできました

--- VhdlExample/Sequencer.vhd
+++ VhdlExample/Sequencer.vhd
@@ -21,6 +21,7 @@ entity Sequencer is
          FlagS : in  STD_LOGIC;
          FlagZ : in  STD_LOGIC;
          Stop  : in  STD_LOGIC;
+         Intr  : in  STD_LOGIC;
          -- CPU内部の制御用に出力
          LiLd  : out  STD_LOGIC;
          DrLd  : out  STD_LOGIC;
@@ -77,6 +78,8 @@ constant STAT25 : stat := "10000000000000000000000000";
 
   signal State : stat; -- State
   signal NxtSt : stat; -- Next State
+  signal DROM  : stat;
+
   signal Jmp   : STD_LOGIC;                     -- JMP
   signal Jz    : STD_LOGIC;                     -- JZ
   signal Jc    : STD_LOGIC;                     -- JC
@@ -92,64 +95,84 @@ constant STAT25 : stat := "10000000000000000000000000";
 begin
 -- State machine
   
-  NxtSt <= STAT00 when (State{0}='1' and Stop='1') or    -- Stop
-                       State{3}='1' or State{4}='1' or   -- LD/.../XOR/SHxx,ST
-                       State{5}='1' or State{7}='1' or   -- JMP,IN
-                       State{8}='1' or State{10}='1' or  -- OUT,CALL
-                       State{11}='1' or State{13}='1' or -- EI/DI,PUSH
-                       State{15}='1' or State{16}='1' or -- POP,RET
-                       State{18}='1' or State{19}='1' or -- RETI,HALT
-                       State{20}='1' else                -- ERROR
-           STAT01 when State{0}='1' and Stop='0'  else   -- Fetch
-           STAT02 when DecSt(1)='1' and Type1='1' else   -- LD/ADD/.../XOR
-           "0011" when (DecSt(1)='1' and OP="1001") or   -- SHIFT
-                       DecSt(2)='1' else                 -- LD/ADD/.../XOR
-           "0100" when DecSt(1)='1' and OP="0010" else   -- ST
-           "0101" when DecSt(1)='1' and OP="1010" else   -- JMP/JZ/JC/JM
-           "0110" when DecSt(1)='1' and OP="1011" else   -- CALL
-           "0111" when DecSt(6)='1'               else
-           "1000" when DecSt(1)='1' and OP="1101" and Rx="00" else -- PUSH
-           "1001" when DecSt(8)='1'               else
-           "1010" when DecSt(1)='1' and OP="1101" and Rx="10" else -- POP
-           "1011" when DecSt(10)='1'                          else
-           "1100" when DecSt(1)='1' and OP="1110" else   -- RET
-           "1101";                                       -- HALT/ERROR
+  NxtSt <=  DROM   when State(1)='1' else
+            STAT00 when (State(0)='1' and Stop='1') or   -- Stop
+                       State(3)='1' or State(4)='1' or   -- LD/.../XOR/SHxx,ST
+                       State(5)='1' or State(7)='1' or   -- JMP,IN
+                       State(8)='1' or State(10)='1' or  -- OUT,CALL
+                       State(11)='1' or State(13)='1' or -- EI/DI,PUSH
+                       State(15)='1' or State(16)='1' or -- POP,RET
+                       State(18)='1' or State(19)='1' or -- RETI,HALT
+                       State(20)='1' or                  -- ERROR
+                       State(25)='1' else                -- Intr               
+           STAT21 when State(0)='1' and Intr='1' and FlagE='1' else -- Stop
+           STAT22 when State(21)='1' else                -- Intr
+           STAT23 when State(22)='1' else                -- Intr
+           STAT24 when State(23)='1' else                -- Intr
+           STAT25 when State(24)='1' else                -- Intr
+           STAT01 when State(0)='1' and Stop='0'  else   -- Fetch
+           STAT03 when State(2)='1' else                 -- LD/ADD/.../XOR
+           STAT07 when State(6)='1'               else   --IN
+           STAT10 when State(9)='1' else                 --CALL
+           STAT13 when State(12)='1' else                --PUSH
+           STAT15 when State(14)='1' else                --POP
+           STAT18 when State(17)='1'                     --RETI
+           ;                                       
 
   process(Clk, Reset)
   begin
     if (Reset='1') then
-      Stat <= "0000";
+      State <= STAT00;
     elsif (Clk'event and Clk='1') then
-      Stat <= NxtSt;
+      State <= NxtSt;
     end if;
   end process;
   
   -- Control Signals
   Jmp  <= '1' when Rd="00" else '0';  -- JMP
-  Jz   <= '1' when Rd="01" else '0';  -- JZ
-  Jc   <= '1' when Rd="10" else '0';  -- JC
-  Jm   <= '1' when Rd="11" else '0';  -- JM
+  Jz   <= '1' when OP(0)='0' and Rd="01" else '0';  -- JZ
+  Jc   <= '1' when OP(0)='0' and Rd="10" else '0';  -- JC
+  Jm   <= '1' when OP(0)='0' and Rd="11" else '0';  -- JM
+  Jnz  <= '1' when OP(0)='1' and Rd="01" else '0';  -- JNZ
+  Jnc  <= '1' when OP(0)='1' and Rd="10" else '0';  -- JNC
+  Jnm  <= '1' when OP(0)='1' and Rd="11" else '0';  -- JNM
   Immd <= '1' when Rx="11" else '0';  -- Immediate mode
   
   --        JMP     JZ and Z Flag       JC and C Flag       JM and S Flag
-  JmpCnd <= Jmp or (Jz and Flag(0)) or (Jc and Flag(2)) or (Jm and Flag(1));
+  JmpCnd <= Jmp or (Jz and FlagZ) or (Jc and FlagC) or (Jm and FlagS) or 
+            (Jnz and not FlagZ) or (Jnc and not FlagC) or (Jnm and not FlagS);
   
-  IrLd  <= DecSt(0);
-  DrLd  <= DecSt(1) or (DecSt(2) and not Immd) or DecSt(10);
-  FlgLd <= '1' when DecSt(3)='1' and OP/="0001" else '0';    -- OP /=LD
-  GrLd  <= '1' when (DecSt(3)='1' and OP/="0101") or         -- OP /=CMP
-           DecSt(11)='1' else '0';
-  SpP1  <= DecSt(10) or DecSt(12);
-  SpM1  <= DecSt(6)  or DecSt(8);
-  PcP1  <= (DecSt(0) and not Stop) or
-           DecSt(2) or DecSt(4) or DecSt(5) or DecSt(6);
-  PcJmp <= (DecSt(5) and JmpCnd) or DecSt(7);
-  PcRet <= DecSt(12);
-  Ma    <= "00" when DecSt(0)='1' or DecSt(1)='1' else       -- "00"=PC
-           "01" when DecSt(2)='1' or DecSt(4)='1' else       -- "01"=EA
+  IrLd  <= State(0);                                         -- Stop
+  DrLd  <= State(1) or                                       -- Fetch
+           (State(2) and not Immd) or                        -- LD/ADD/.../XOR
+           State(6) or State(14);                            -- IN, POP
+  FlgLdA <= '1' when State(3)='1' and OP/="0001" else '0';   -- OP /=LD
+  FlgLdM <= State(17);                                       -- RETI
+  GrLd  <= '1' when (State(3)='1' and OP/="0101") or         -- OP /=CMP
+           State(7)='1' or                                   -- IN
+           State(15)='1' else '0';                           -- POP
+  SpP1  <= State(14) or State(16) or State(17);              -- POP, RET, RETI
+  SpM1  <= State(9)  or State(12) or                         -- CALL, PUSH
+           Stete(21) or State(23);                           -- Intr
+  PcP1  <= (State(0) and not Stop) or                        -- Stop
+           State(2) or                                       -- LD/ADD/.../XOR
+           State(4) or (State(5) and not JmpCnd) or          -- ST, JMP
+           State(6) or State(8) or State(9);                 -- IN, OUT, CALL
+  PcJmp <= (State(5) and JmpCnd) or State(10);               -- JMP, CALL
+  PcRet <= State(16) or State(18) or                         -- RET, RETI
+           State(24) or State(25);                           -- Intr
+  Ma    <= "00" when State(0)='1' or State(1)='1' or
+                     State(25)='1' else                      -- "00"=PC
+           "01" when State(2)='1' or State(4) or 
+                     State(6)='1' else                       -- "01"=EA
            "10";                                             -- "10"=SP
-  Md    <= not DecSt(7);
-  We    <= DecSt(4)  or DecSt(7) or DecSt(9);
-  Halt  <= DecSt(13);
+  Md    <= "00" when State(10)='1' or State(22)='1' else     -- "00"=PC
+           "01" when State(23)='1' else                      -- "01"=FLAG
+           "10";                                             -- "10"=GR
+  We    <= State(4)  or State(8) or                          -- ST, OUT
+           State(10) or State(13) or                         -- CALL, PUSH
+           State(22) or State(23);                           -- Intr
+  Halt  <= State(19) or State(20);                           -- HALT, ERROR
+  Err   <= State(20);                                        -- ERROR
 
 end Behavioral;
